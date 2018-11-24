e18a826a6f2f9ab8abfbd63dfc8554dcb9b6fe8f
Jim MOUSSU <jmoussu@e1r10p20.42.fr>
2018-11-24 11:46:33 +0100

    last push avec main lstmap

--- ft_strrealloc.c
+++ ft_strrealloc.c
@@ -6,7 +6,7 @@
 /*   By: jmoussu <jmoussu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/22 18:15:17 by jmoussu           #+#    #+#             */
-/*   Updated: 2018/11/22 18:33:17 by jmoussu          ###   ########.fr       */
+/*   Updated: 2018/11/23 19:59:35 by jmoussu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_strrealloc(int len, char *str)
 {
 	char *newstr;
 
-	if (!(newstr = (char *)malloc(sizeof(char) * (strlen(str) + len + 1))))
+	if (!(newstr = (char *)malloc(sizeof(char) * (ft_strlen(str) + len + 1))))
 		return (NULL);
 	ft_bzero(newstr, strlen(str) + len + 1);
 	ft_strcpy(newstr, str);
